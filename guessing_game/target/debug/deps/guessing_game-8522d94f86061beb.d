467c49cfa04b340ab4fbd20e782da3ef102a51bc
Palm <241it002i@s.internet.ac.jp>
2025-09-18 23:10:13 +0900

    Remove target directory from version control

--- guessing_game/target/debug/deps/guessing_game-8522d94f86061beb.d
+++
@@ -1,5 +0,0 @@
-/Users/uematsuayaka/Documents/example_Rust/TheRustProgrammingLanguage/guessing_game/target/debug/deps/guessing_game-8522d94f86061beb.d: src/main.rs
-
-/Users/uematsuayaka/Documents/example_Rust/TheRustProgrammingLanguage/guessing_game/target/debug/deps/guessing_game-8522d94f86061beb: src/main.rs
-
-src/main.rs:
